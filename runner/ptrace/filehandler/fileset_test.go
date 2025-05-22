cc449687565d83c8c9b41fb63de2b820ae010756
DNEGEL3125 <2839212140@qq.com>
2025-05-22 13:26:41 +0800

    test(FileSet): add unit test for Add

--- runner/ptrace/filehandler/fileset_test.go
+++ runner/ptrace/filehandler/fileset_test.go
@@ -35,3 +35,55 @@ func TestFileSet_IsInSetSmart(t *testing.T) {
 		})
 	}
 }
+
+// Unit test for Add method
+func TestFileSet_Add(t *testing.T) {
+	// Create a new FileSet
+	fs := NewFileSet()
+
+	if fs.SystemRoot {
+		t.Errorf("NewFileSet() failed; expected SystemRoot to be false")
+	}
+
+	// Test adding a path that is not the root directory
+	fs.Add("/path/to/file")
+	if fs.SystemRoot {
+		t.Errorf("Add(\"/path/to/file\") failed; expected SystemRoot to be false")
+	}
+
+	// Test adding the root directory
+	fs.Add("/")
+	if !fs.SystemRoot {
+		t.Errorf("Add(\"/\") failed; expected SystemRoot to be true")
+	}
+
+	// Test adding a regular path
+	fs.Add("/path/to/file")
+	if !fs.Set["/path/to/file"] {
+		t.Errorf("Add(\"/path/to/file\") failed; expected path to be in the set")
+	}
+
+	// Test adding another path
+	fs.Add("/another/path")
+	if !fs.Set["/another/path"] {
+		t.Errorf("Add(\"/another/path\") failed; expected path to be in the set")
+	}
+
+	// Test adding a path with a trailing slash
+	fs.Add("/path/to/dir/")
+	if !fs.Set["/path/to/dir/"] {
+		t.Errorf("Add(\"/path/to/dir/\") failed; expected path to be in the set")
+	}
+
+	// Test adding a path with a wildcard
+	fs.Add("/path/to/dir/*")
+	if !fs.Set["/path/to/dir/*"] {
+		t.Errorf("Add(\"/path/to/dir/*\") failed; expected path to be in the set")
+	}
+
+	// Test adding a relative path
+	fs.Add("relative/path")
+	if !fs.Set["relative/path"] {
+		t.Errorf("Add(\"relative/path\") failed; expected path to be in the set")
+	}
+}
